791c0ff0b2a1cb3c9307e0ad4cb2e935d2ce182c
karol <>
2022-02-28 08:56:07 +0100

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# terraform-aws-vpc-global
+Terraform module whch creates AVX Spoke Gateway resource on AWS.
---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "spoke_gateway" {
+  description = "The created Aviatrix spoke gateway as an object with all of it's attributes."
+  value       = aviatrix_spoke_gateway.default
+}
